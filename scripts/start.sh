16f4921ca42abc87b2b3adb0907add91928031e0
colinwd <me@colinwd.com>
2017-03-03 21:41:20 -0800

    fix up start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
 source ~/.yosenv/bin/activate
 
 cd /yosbot
 pip install -r requirements.txt
+
+aws s3 cp s3://yosbot/config/rtmbot.conf /yosbot/rtmbot.conf
+aws s3 cp s3://yosbot/config/weather.conf /yosbot/weather.conf
 
 nohup rtmbot &>/dev/null &
